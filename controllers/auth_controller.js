a8f8374dc58d0d03c788b529aa52379f654a0918
Deepak Bawa <bawa_d@ymail.com>
2019-02-22 16:51:44 +0530

    Update auth_controller.js

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,67 +1,88 @@
-var express = require('express');
 const jwt = require('jsonwebtoken');
-var User = require('../models').User;
+const User = require('../models').User;
+const accesses = require('../config/access');
 
-var signin = ( req, res, next ) => {
+module.exports = class LeadController {
 
-    var email = req.body.email;
+    constructor(){}
 
-    User.find({ where: { email: email } })
-        .then((result) => {
+    signin( req, res, next ) {
 
-            if (result === null) {
-
-                res.status(200).send({ err: ["Record not found!"] });
-            } else {
-
-                let userData = {
-                    "id": result.id,
-                    "firstName": result.firstName,
-                    "lastName": result.lastName,
-                    "email": result.email
-                };
-
-                jwt.sign({userData},'SuperSecRetKey', { expiresIn: 60 * 60 }, (err, token) => {
-                    res.status(200).send({ err: [], token: 'bearer '+ token });
-                });
-            }
-        })
-        .catch((error) => {
-
-            res.status(200).send({ err: error });
-        });
-};
-
-var verifyToken = (req, res, next) => {
+        var email = req.body.email;
     
-    //Request header with authorization key
-    const bearerHeader = req.headers['authorization'];
+        User.find({ where: { email: email } })
+            .then((result) => {
     
-    //Check if there is  a header
-    if(typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
+                if (result === null) {
+    
+                    res.status(200).send({ err: ["Record not found!"] });
+                } else {
+    
+                    let userData = {
+                        "id": result.id,
+                        "firstName": result.firstName,
+                        "lastName": result.lastName,
+                        "email": result.email
+                    };
+    
+                    jwt.sign({userData},'SuperSecRetKey', { expiresIn: 60 * 60 }, (err, token) => {
+                        res.status(200).send({ err: [], token: 'bearer '+ token });
+                    });
+                }
+            })
+            .catch((error) => {
+    
+                res.status(200).send({ err: error });
+            });
+    }
+    
+    verifyToken(req, res, next) {
+        //Request header with authorization key
+        const bearerHeader = req.headers['authorization'];
         
-        //Get Token arrray by spliting
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
+        //Check if there is  a header
+        if(typeof bearerHeader !== 'undefined') {
+            const bearer = bearerHeader.split(' ');
+            
+            //Get Token arrray by spliting
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
+    
+            jwt.verify(req.token, 'SuperSecRetKey', (err, authData)=>{
+    
+                if(err){
+                    res.status(403).send({ err: err });
+                }else{
+                    
+                    req.authData = authData;
+                    //call next middleware
+                    next();
+                }
+            });
+        }else{
+            res.status(403).send({ err: 'Header is not defined.' });            
+        }
+    }
 
-        jwt.verify(req.token, 'SuperSecRetKey', (err, authData)=>{
+    verifyAccess( req, res, next ) {
 
-            if(err){
-                res.status(403).send({ err: err });
-            }else{
-                
-                req.authData = authData;
+        let full_path = req.baseUrl + req.route.path;
+        // let roleId = req.authData.roleId;
+        let roleId = 2;
+
+        if( accesses.hasOwnProperty( full_path ) ) {
+
+            let authorisedRoles = accesses[full_path];
+            if ( authorisedRoles.indexOf( roleId ) > -1 ) {
                 //call next middleware
                 next();
+            } else {
+                // Unauthorized Access
+                res.status(403).send({ err: 'Unauthorized access.' });
             }
-        });
-    }else{
-        res.sendStatus(403);
+        } else {
+            // Path is not declared in the /config/access.json
+            res.status(403).send({ err: 'Path is not declared in the /config/access.json' });
+        }
     }
-};
-
-module.exports = {
-    signin: signin,
-    verifyToken: verifyToken
-};
+}
